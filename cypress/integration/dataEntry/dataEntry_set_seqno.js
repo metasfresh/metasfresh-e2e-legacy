a0f1531eaf08b93ae56fe8c7f2d7b9e3b6c8b12f
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-05-13 17:47:40 +0200

    5162-app - Dataentry via Rest-API
    work on adopting tests to changed naming
    https://github.com/metasfresh/metasfresh/issues/5162

--- cypress/integration/dataEntry/dataEntry_set_seqno.js
+++ cypress/integration/dataEntry/dataEntry_set_seqno.js
@@ -1,22 +1,22 @@
-import { DataEntryGroup } from '../../support/utils/dataEntryGroup';
+import { DataEntryTab } from '../../support/utils/DataEntryTab';
 
 describe('Reproduce issue https://github.com/metasfresh/metasfresh-webui-frontend/issues/2214', function() {
-  it('Create dataEntry group with SeqNo 21', function() {
+  it('Create dataEntry tab with SeqNo 21', function() {
     const timestamp = new Date().getTime(); // used in the document names, for ordering
-    const dataEntryGroupName = `Group1 ${timestamp}`;
+    const dataEntryTabName = `Tab1 ${timestamp}`;
 
-    new DataEntryGroup(dataEntryGroupName, 'Business Partner')
-      .setTabName('Group1-Tab1')
+    new DataEntryTab(dataEntryTabName, 'Business Partner')
+      .setTabName('Tab1')
       .setSeqNo(21)
       .apply();
 
     // these are sortof guards, to demonstrate that other fields work.
     cy.get('.form-field-Name')
       .find('input')
-      .should('have.value', dataEntryGroupName);
+      .should('have.value', dataEntryTabName);
     cy.get('.form-field-TabName')
       .find('input')
-      .should('have.value', 'Group1-Tab1');
+      .should('have.value', 'Tab1');
 
     // here it comes: SeqNo has a value of '21', as entered by us
     cy.get('.form-field-SeqNo')

beb4ca481d23eb296d13459ec5657e8b0f8a0290
Olly <pisollie94@gmail.com>
2019-06-20 10:39:52 +0300

    test done

--- cypress/integration/finance/create_dunning_candidates_spec.js
+++ cypress/integration/finance/create_dunning_candidates_spec.js
@@ -29,27 +29,37 @@ describe('create dunning candidates', function() {
         .setProductCategory(productCategoryValue + '_' + productCategoryName)
         .apply();
     });
-    it('Set packing item', function() {
-      cy.selectTab('M_HU_PI_Item_Product')
-        .pressAddNewButton()
-        .selectInListField('M_HU_PI_Item_ID', productPacking, true /*modal*/)
-        .clearField('Qty')
-        .writeIntoStringField('Qty', '10', true /*modal*/)
-        .selectDateViaPicker('ValidFrom', 'today', true /*modal*/)
-        .pressDoneButton();
-    });
+
+    cy.selectTab('M_HU_PI_Item_Product')
+      .pressAddNewButton()
+      .selectInListField('M_HU_PI_Item_ID', 'IFCO 6410', true /*modal*/)
+      .clearField('Qty')
+      .writeIntoStringField('Qty', '10', true /*modal*/)
+      .selectDateViaPicker('ValidFrom', 'today', true /*modal*/)
+      .pressDoneButton();
+
     cy.fixture('sales/simple_customer.json').then(customerJson => {
       Object.assign(new BPartner(), customerJson)
         .setName(customerName)
         .apply();
     });
 
+    cy.selectTab('Customer');
+    cy.get('.tr-even > :nth-child(3)')
+      .dblclick()
+      .selectInListField('C_PaymentTerm_ID', 'immediately', false, null, true);
+    cy.selectTab('Customer');
+    cy.get('.tab-pane > :nth-child(1) > :nth-child(1) > :nth-child(1) > .panel').scrollTo('right');
+    cy.get('.tr-even > :nth-child(9)')
+      .dblclick()
+      .selectInListField('C_Dunning_ID', 'test Dunning test', false, null, true);
+
     new SalesInvoice(customerName, salesInvoiceTargetDocumentType)
       .addLine(
         new SalesInvoiceLine()
           .setProduct(productName)
           .setQuantity(20)
-          .setPackingItem(productPacking)
+          .setPackingItem('IFCO 6410 x 10 Stk')
           .setTuQuantity(2)
       )
       .setDocumentAction('Complete')
@@ -57,7 +67,11 @@ describe('create dunning candidates', function() {
       .apply();
   });
 
-  it('only check', function() {
-    cy.visit('/window/143');
+  it('create dunning candidates', function() {
+    cy.visit('/window/540154');
+    cy.waitForHeader('Finance', 'Dunning Candidates');
+    cy.executeHeaderAction('C_Dunning_Candidate_Create');
+    cy.get('.panel-modal-header > .items-row-2 > :nth-child(2)').click();
+    cy.get(':nth-child(3) > .header-btn > .header-item-container > .header-item').click();
   });
 });

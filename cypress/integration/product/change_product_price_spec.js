8edbc42360724a3c847e29ea962dffae5bc8f7e5
Olly <pisollie94@gmail.com>
2019-07-02 20:12:31 +0300

    WIP, final tests and jenkins test remains to be done. Flow done as shown in video.

--- cypress/integration/product/change_product_price_spec.js
+++ cypress/integration/product/change_product_price_spec.js
@@ -1,5 +1,5 @@
-import { Product, ProductCategory } from '../../support/utils/product';
-import { BPartner, BPartnerLocation } from '../../support/utils/bpartner';
+import { Product, ProductCategory, ProductPrice } from '../../support/utils/product';
+import { BPartner } from '../../support/utils/bpartner';
 import { SalesOrder } from '../../support/utils/sales_order';
 import { toggleNotFrequentFilters, selectNotFrequentFilterWidget, applyFilters } from '../../support/functions';
 
@@ -10,8 +10,11 @@ describe('change product price', function() {
   const productCategoryName = `ProductCategoryName ${timestamp}`;
   const productCategoryValue = `ProductNameValue ${timestamp}`;
   const bpartnerName = `Testcustomer ${timestamp}`;
-  const bpartnerLocation = `Testadresse ${timestamp}`;
   const salesReference = `TestSalesOrder ${timestamp}`;
+  const listPrice = 1.23;
+  const standardPrice1 = 2.23;
+  const standardPrice2 = 4.23;
+  const limitPrice = 3.23;
 
   before(function() {
     cy.fixture('product/simple_productCategory.json').then(productCategoryJson => {
@@ -20,23 +23,26 @@ describe('change product price', function() {
         .setValue(productCategoryValue)
         .apply();
     });
+    let productPrice;
+    cy.fixture('product/product_price.json').then(productPriceJson => {
+      productPrice = Object.assign(new ProductPrice(), productPriceJson)
+        .setListPriceAmount(listPrice)
+        .setStandardPriceAmount(standardPrice1)
+        .setLimitPriceAmount(limitPrice);
+    });
     cy.fixture('product/product.json').then(productJson => {
       Object.assign(new Product(), productJson)
         .setName(productName)
         .setValue(productValue)
-        // .setProductType('Service')
         .setProductCategory(productCategoryValue + '_' + productCategoryName)
-        .setProductCategory('24_Gebinde')
+        .addProductPrice(productPrice)
         .apply();
     });
     cy.fixture('sales/simple_bpartner.json').then(bpartner => {
-      Object.assign(new BPartner(), bpartner).apply();
+      Object.assign(new BPartner(), bpartner)
+        .setName(bpartnerName)
+        .apply();
     });
-    // new BPartner()
-    //   .setName(bpartnerName)
-    //   .setCustomer(true)
-    //   .addLocation(new BPartnerLocation(bpartnerLocation).setCity('Cologne').setCountry('Deutschland'))
-    //   .apply();
     new SalesOrder(salesReference)
       .setBPartner(bpartnerName)
       .setPricingSystem('Testpreisliste Kunden')
@@ -52,8 +58,17 @@ describe('change product price', function() {
     cy.writeIntoStringField('Qty', '1');
   });
 
-  it('check in product', function() {
-    cy.visit('/window/140');
+  it('check price', function() {
+    cy.get('.table-flex-wrapper table tbody tr')
+      .should('have.length', 1)
+      .click();
+    cy.openAdvancedEdit();
+    cy.get(`.form-field-PriceEntered`).find('input', standardPrice1);
+    cy.pressDoneButton();
+  });
+
+  it('add another price', function() {
+    cy.visitWindow('140');
     cy.waitForHeader('Product Management', 'Product');
     toggleNotFrequentFilters();
     selectNotFrequentFilterWidget('default');
@@ -63,5 +78,39 @@ describe('change product price', function() {
     cy.get('.table > tbody')
       .contains('td', productName)
       .dblclick();
+
+    cy.selectTab('M_ProductPrice');
+    cy.get('.table-flex-wrapper table tbody tr')
+      .should('have.length', 1)
+      .click();
+    cy.openAdvancedEdit();
+
+    cy.writeIntoStringField('PriceStd', standardPrice2, true /*modal*/, null /*rewriteUrl*/, true /*noRequest*/);
+
+    cy.get('.btn')
+      .contains('Done')
+      .should('exist')
+      .click()
+      .wait(1000);
+  });
+
+  it('check in sales order new product price', function() {
+    cy.visitWindow('143');
+    toggleNotFrequentFilters();
+    selectNotFrequentFilterWidget('default');
+    cy.writeIntoStringField('Name', bpartnerName, false, undefined, true);
+    applyFilters();
+
+    cy.selectTab('C_OrderLine');
+
+    cy.pressBatchEntryButton();
+    cy.writeIntoLookupListField('M_Product_ID', productName, productName);
+    cy.writeIntoStringField('Qty', '1');
+    cy.get('.table-flex-wrapper table tbody tr')
+      .should('have.length', 2)
+      .click();
+    cy.openAdvancedEdit();
+    cy.get(`.form-field-PriceEntered`).find('input', standardPrice2);
+    cy.pressDoneButton();
   });
 });

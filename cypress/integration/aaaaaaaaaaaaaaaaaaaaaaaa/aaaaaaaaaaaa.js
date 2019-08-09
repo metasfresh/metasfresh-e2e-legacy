c03df352e930351f3f0c9c90a3e6cc1707c1beea
TheBestPessimist <cristian@tbp.land>
2019-08-09 09:07:17 +0300

    Create ProductCategory, Product and PriceListSchema
    https://github.com/metasfresh/metasfresh-e2e/issues/286

--- cypress/integration/aaaaaaaaaaaaaaaaaaaaaaaa/aaaaaaaaaaaa.js
+++ cypress/integration/aaaaaaaaaaaaaaaaaaaaaaaa/aaaaaaaaaaaa.js
@@ -1,7 +1,8 @@
 import { humanReadableNow } from '../../support/utils/utils';
 import { Pricesystem } from '../../support/utils/pricesystem';
 import { PriceList } from '../../support/utils/pricelist';
-import { Product, ProductCategory, ProductPrice } from '../../support/utils/product';
+import { Product, ProductCategory } from '../../support/utils/product';
+import { PriceListSchema } from '../../support/utils/price_list_schema';
 
 // these variables are at the root of the file
 const date = humanReadableNow();
@@ -13,16 +14,23 @@ let fixture;
 let priceSystemName;
 let priceListName;
 
-// // Product
-// let categoryName;
-// let productName1;
-// let productName2;
-// let productType;
+// Product
+let categoryName;
+let productName1;
+let productName2;
+
+// Price List Schema
+let priceListSchemaName;
+let surchargeAmount;
 
 // test
 let priceListID;
 
 describe('Read fixture and prepare test data', function() {
+  it('reaaaaaalyyyyyyyyy', function() {
+    cy.wait(5000);
+  });
+
   it('Read fixture', function() {
     cy.fixture('price/add_a_product_to_a_pricelist_schema_and_create_a_new_PLV.json').then(fixtureJson => {
       fixture = fixtureJson;
@@ -43,14 +51,43 @@ describe('Read fixture and prepare test data', function() {
     cy.getCurrentWindowRecordId().then(id => (priceListID = id));
   });
 
-  // it('Create Product1 and Category', function() {
-  //   const productCategoryJson = fixture['ProductCategory'];
-  //   productCategoryJson.name = productCategoryJson + appendDate();
-  //   categoryName = productCategoryJson.name;
-  //
-  //
-  //   Builder2.createProductEntities(productCategoryJson, priceListName, productName1, productName1, productType);
-  // });
+  it('Create Product1 and Category', function() {
+    const productCategoryJson = fixture['ProductCategory'];
+    productCategoryJson.name = productCategoryJson.name + appendDate();
+    categoryName = productCategoryJson.name;
+
+    const productJson = fixture['Products'][0];
+    productJson.name = productJson.name + appendDate();
+    productName1 = productJson.name;
+
+    productJson.productPrices[0].priceList = priceListName;
+
+    Builder2.createProductCategory(productCategoryJson);
+    Builder2.createProduct(productJson);
+  });
+
+  it('Create Product2', function() {
+    const productJson = fixture['Products'][1];
+    productJson.name = productJson.name + appendDate();
+    productName2 = productJson.name;
+
+    productJson.productPrices[0].priceList = priceListName;
+
+    Builder2.createProduct(productJson);
+  });
+});
+
+describe('Create Price List Schema for Product', function() {
+  it('Create Price List Schema', function() {
+    const priceListSchemaJson = fixture['PriceListSchema'];
+    priceListSchemaJson.name = priceListSchemaJson.name + appendDate();
+    priceListSchemaName = priceListSchemaJson.name;
+
+    surchargeAmount = priceListSchemaJson.lines[0].surchargeAmount;
+    priceListSchemaJson.lines[0].product = productName1;
+
+    Object.assign(new PriceListSchema(), priceListSchemaJson).apply();
+  });
 });
 
 function appendDate() {
@@ -58,26 +95,13 @@ function appendDate() {
 }
 
 class Builder2 {
-  // static createProductEntities(productCategoryJson) {
-  //   cy.fixture('product/simple_productCategory.json').then(productCategoryJson => {
-  //     Object.assign(new ProductCategory(), productCategoryJson).apply();
-  //   });
-  //
-  //   let productPrice;
-  //   cy.fixture('product/product_price.json').then(productPriceJson => {
-  //     productPrice = Object.assign(new ProductPrice(), productPriceJson).setPriceList(priceListName);
-  //   });
-  //
-  //   cy.fixture('product/simple_product.json').then(productJson => {
-  //     Object.assign(new Product(), productJson)
-  //       .setName(productName)
-  //       .setValue(productValue)
-  //       .setProductType(productType)
-  //       .setProductCategory(productCategoryValue + '_' + productCategoryName)
-  //       .addProductPrice(productPrice)
-  //       .apply();
-  //   });
-  // }
+  static createProduct(productJson) {
+    Object.assign(new Product(), productJson).apply();
+  }
+
+  static createProductCategory(productCategoryJson,) {
+    Object.assign(new ProductCategory(), productCategoryJson).apply();
+  }
 
   static createPriceEntities(priceSystemJson, priceListJson) {
     Object.assign(new Pricesystem(), priceSystemJson).apply();
@@ -86,5 +110,4 @@ class Builder2 {
       .setPriceSystem(priceSystemJson.name)
       .apply();
   }
-
 }
